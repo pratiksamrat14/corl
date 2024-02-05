a58044e8e1115e1e2069f392b9c465351402e723
pratik.samrat <pratik.samrat@officebox.local>
2024-02-05 12:16:00 +0530

    Updates

---
+++ static/js/104.a10e7d3e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcorl_tech=self.webpackChunkcorl_tech||[]).push([[104],{2349:(e,c,t)=>{t.r(c),t.d(c,{default:()=>r});var n=t(697),s=t(7689),l=t(184);const r=()=>(0,l.jsx)(n.Z,{sx:()=>({padding:1,display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}),children:(0,l.jsx)(s.j3,{})})}}]);
+//# sourceMappingURL=104.a10e7d3e.chunk.js.map
---
+++ static/js/551.6a0940c9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcorl_tech=self.webpackChunkcorl_tech||[]).push([[551],{6551:(e,c,t)=>{t.r(c),t.d(c,{default:()=>r});var n=t(697),s=t(7689),l=t(184);const r=()=>(0,l.jsx)(n.Z,{sx:()=>({padding:1,display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}),children:(0,l.jsx)(s.j3,{})})}}]);
+//# sourceMappingURL=551.6a0940c9.chunk.js.map
